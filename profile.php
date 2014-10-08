12faef0b7cc8fc386ad44eb9b95f50642e5dc4f0
mistastone <michael.stone2@gmail.com>
2014-10-07 21:36:23 -0400

    Started Profile page

---
+++ profile.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mento: Profile</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700,400' rel='stylesheet' type='text/css'>
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+	<header class="profile">
+		<a href="index.html"><h1>Mento</h1></a>
+		<ul>
+			<li>Sign Out</li>
+			<li>Browse</li>
+			<li>Profile</li>
+		</ul>
+	</header>
+
+	<section class="profiletop">
+
+		<div class="profileimage">
+			<img src="assets/images/profile.jpg" alt="Profile Image">
+		</div>
+
+	
+		<div class="profilename">
+			<h2>Michael Stone</h2>
+			<h3>Web Developer <span>(Toronto)</span></h3>
+		</div>
+
+	</section>
+
+	<section class ="skills">
+		<div class="havelearned">
+			<h3>I Am Offering These Skills...</h3>
+			<ul>
+				<li>HTML5</li>
+				<li>CSS3</li>
+				<li>Responsive Design</li>
+				<li>JavaScript</li>
+				<li>jQuery</li>
+			</ul>
+		</div>
+
+		<div class="learning">
+			<h3>I Am Looking For These Skills...</h3>
+			<ul>
+				<li>PHP</li>
+				<li>Angular</li>
+				<li>Node.js</li>
+				<li>Ruby</li>
+				<li>Jade</li>
+			</ul>
+		</div>
+	</section>
+		
+	
+</body>
+</html>
